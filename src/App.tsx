f55c60ab4da1969021ad7f3fe08e2487c889c785
Sene4kaaa <senkevich92@mail.ru>
2023-01-31 17:28:45 +0300

    Add grid for App.css

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,55 @@
 import React from 'react';
 import './App.css';
-import Technologies from "./Technologies";
-import Header from "./Header";
-import Footer from "./Footer";
+
 
 const App = () => {
     return (
-        <div>
-            <Header/>
-            <Technologies/>
-            <Footer/>
-
+        <div className={'app-wrapper'}>
+            <header className={'header'}>
+                <img src={'https://cdn.dribbble.com/users/1790995/screenshots/5089886/dfsdfsdfsdf_4x.jpg'}/>
+            </header>
+            <nav className={'nav'}>
+                <div>
+                    <a>Profile</a>
+                </div>
+                <div>
+                    <a>Message</a>
+                </div>
+                <div>
+                    <a>News</a>
+                </div>
+                <div>
+                    <a>Music</a>
+                </div>
+                <div>
+                    <a>Settings</a>
+                </div>
+            </nav>
+            <div className={'content'}>
+                <div>
+                    <img src={'https://look.com.ua/pic/202010/640x480/look.com.ua-362136.jpg'}/>
+                </div>
+                <div>
+                    ava + description
+                </div>
+                <div>
+                    My posts
+                    <div>
+                        New post
+                    </div>
+                    <div>
+                        <div>
+                            Post 1
+                        </div>
+                        <div>
+                            Post 2
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
-
-
-
 
 
 export default App;
