26ebfc2fc7cc80b98f61eab4fe495c4c6582fd12
Sene4kaaa <senkevich92@mail.ru>
2023-01-31 21:28:54 +0300

    Add props in Post.tsx

--- src/components/Profile/MyPosts/Post/Post.tsx
+++ src/components/Profile/MyPosts/Post/Post.tsx
@@ -1,14 +1,18 @@
 import React from "react";
 import s from './Post.module.css';
 
+type PostPropsType = {
+    message: string,
+    likeCounts:number
+}
 
-const Post = () => {
+const Post = (props: PostPropsType) => {
     return (
         <div className={s.item}>
             <img src={'https://w7.pngwing.com/pngs/862/646/png-transparent-beard-hipster-male-man-avatars-xmas-giveaway-icon-thumbnail.png'}/>
-            Post 1
+            {props.message}
             <div>
-                <span>like</span>
+                <span>{props.likeCounts}</span>
             </div>
         </div>
     )
