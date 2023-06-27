9e0ebae939707ada381c2ef3859d8f8a87cc2edc
Sene4kaaa <senkevich92@mail.ru>
2023-06-27 16:05:41 +0300

    Refactoring code Add DAL

---
+++ src/api/api.ts
@@ -0,0 +1,24 @@
+import axios from "axios";
+
+
+// const baseUrl = 'https://social-network.samuraijs.com/api/1.0/'
+
+const instance = axios.create({
+    withCredentials: true,
+    baseURL: 'https://social-network.samuraijs.com/api/1.0/'
+    // headers: {
+    //     "API-KEY": "e5cf8594-32cf-4be5-9a53-8fb67649cdee"
+    // }
+})
+
+
+export const usersAPI = {
+    getUsers(currentPage: number, pageSize: number) {
+        return instance
+            .get(`users?page=${currentPage}&count=${pageSize}`)
+            .then(response => {
+                return response.data
+            })
+    }
+}
+
