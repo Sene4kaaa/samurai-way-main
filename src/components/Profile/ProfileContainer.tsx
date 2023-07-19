88526b4027b01b926401714116e04707a0c0fbef
Sene4kaaa <senkevich92@mail.ru>
2023-07-19 17:47:46 +0300

    Refactoring code

--- src/components/Profile/ProfileContainer.tsx
+++ src/components/Profile/ProfileContainer.tsx
@@ -35,6 +35,9 @@ class ProfileContainer extends React.Component<PropsType, {}> {
         let userId = this.props.match.params.userId
         if (!userId) {
             userId = this.props.authorizedUserId
+            if (!userId) {
+                this.props.history.push('/login')
+            }
         }
         this.props.getUserProfileTC(userId)
         this.props.getStatusTC(userId)
