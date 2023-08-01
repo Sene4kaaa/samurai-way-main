48512a62997891bf911a064ec8459c53b6301a5e
Sene4kaaa <senkevich92@mail.ru>
2023-08-01 19:06:14 +0300

    Refactoring code Add Paginator.tsx

---
+++ src/components/Users/Paginator.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import styles from "./users.module.css";
+
+
+type PaginatorPropsType = {
+    totalUsersCount: number
+    pageSize: number
+    currentPage: number
+    onPageChanged: (p: number) => void
+
+}
+
+export const Paginator = (props: PaginatorPropsType) => {
+
+    let pagesCount = Math.ceil(props.totalUsersCount / props.pageSize)
+    let pages = []
+    for (let i = 1; i <= pagesCount; i++)
+        pages.push(i)
+
+    return <div>
+        {pages.map(p => {
+            return <span key={p} className={props.currentPage === p ? styles.selectedPage : ''}
+                         onClick={(e) => {
+                             props.onPageChanged(p)
+                         }}>{p}</span>
+        })}
+    </div>
+}
