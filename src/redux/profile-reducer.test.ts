f5460ba0cdcdf4623ab65639300ec83eeb40dfcf
Sene4kaaa <senkevich92@mail.ru>
2023-08-01 16:36:26 +0300

    Refactoring code Add test in profile-reducer.test.ts

--- src/redux/profile-reducer.test.ts
+++ src/redux/profile-reducer.test.ts
@@ -1,18 +1,20 @@
 import {addPostActionCreator, PostType, profileReducer, ProfileType} from "./profile-reducer";
 
+let state = {
+    posts: [
+        {id: 1, message: 'Hi, how are you?', likesCounts: 12},
+        {id: 2, message: 'it\'s my first post', likesCounts: 11},
+        {id: 3, message: 'BlaBla', likesCounts: 15},
+        {id: 4, message: 'Dada', likesCounts: 20},
+    ] as PostType[],
+    profile: null as null | ProfileType,
+    status: ''
+}
+
 test('length of posts should be incremented', cb => {
     // 1 test data
     let action = addPostActionCreator('BMW')
-    let state = {
-        posts: [
-            {id: 1, message: 'Hi, how are you?', likesCounts: 12},
-            {id: 2, message: 'it\'s my first post', likesCounts: 11},
-            {id: 3, message: 'BlaBla', likesCounts: 15},
-            {id: 4, message: 'Dada', likesCounts: 20},
-        ] as PostType[],
-        profile: null as null | ProfileType,
-        status: ''
-    }
+
 
     // 2 action
     let newState = profileReducer(state,action)
@@ -20,3 +22,16 @@ test('length of posts should be incremented', cb => {
     // 3 expectation
     expect(newState.posts.length).toBe(5)
 })
+
+test('message of new post should be BMW', cb => {
+    // 1 test data
+    let action = addPostActionCreator('BMW')
+
+
+    // 2 action
+    let newState = profileReducer(state,action)
+
+    // 3 expectation
+    expect(newState.posts[4].message).toBe('BMW')
+})
+
