33e014bbe7db1aba9e91fc69886469e5c346746f
Sene4kaaa <senkevich92@mail.ru>
2023-08-03 16:11:14 +0300

    Refactoring code Add in BrowserRouter basename

--- src/index.tsx
+++ src/index.tsx
@@ -6,7 +6,6 @@ import {BrowserRouter} from "react-router-dom";
 import {Provider} from "react-redux";
 import {store} from "./redux/redux-store";
 
-
 ReactDOM.render(
     <BrowserRouter basename={process.env.PUBLIC_URL}>
         <Provider store={store}>
