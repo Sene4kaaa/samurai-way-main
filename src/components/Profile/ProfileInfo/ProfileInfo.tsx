4764fe05135b312f4d8381af6ffd09bbdf5ad080
Sene4kaaa <senkevich92@mail.ru>
2023-08-06 19:05:21 +0300

    Refactoring code

--- src/components/Profile/ProfileInfo/ProfileInfo.tsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.tsx
@@ -70,7 +70,7 @@ type ContactPropsType = {
     contactValue: string
 }
 const Contact = (props: ContactPropsType) => {
-    return <div><b>{props.contactTitle}</b>: {props.contactValue}</div>
+    return <div className={s.contact}><b>{props.contactTitle}</b>: {props.contactValue}</div>
 }
 
 
