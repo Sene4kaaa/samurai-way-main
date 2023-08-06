9c4ead819f4f2fc56d4f842ebd46d3f02a40888a
Sene4kaaa <senkevich92@mail.ru>
2023-08-06 21:01:01 +0300

    Refactoring code

--- src/components/Profile/ProfileInfo/ProfileDataForm.tsx
+++ src/components/Profile/ProfileInfo/ProfileDataForm.tsx
@@ -1,15 +1,62 @@
 import {ProfileContactsPropsType, ProfileType} from "../../../redux/profile-reducer";
 import s from "./ProfileInfo.module.css";
 import React from "react";
+import {FormikProvider, useFormik} from "formik";
+import {login} from "../../../redux/auth-reducer";
 
 type ProfileDataFormPropsType = {
-   profile: ProfileType
+    profile: ProfileType
 }
 
 export const ProfileDataForm = (props: ProfileDataFormPropsType) => {
+
+
+    const formik = useFormik({
+        initialValues: {
+            fullName: '',
+            password: '',
+            rememberMe: false
+        },
+        onSubmit: (values, {setStatus}) => {
+            formik.resetForm();
+        },
+    });
+
     return (
         <div>
-         Form
+            <FormikProvider value={formik}>
+                <form onSubmit={formik.handleSubmit}>
+                    <div>
+                        <button onClick={() => {
+                        }}>save
+                        </button>
+                    </div>
+                    <div>
+                        <b>Full name</b>: <div>
+                        <input
+                            placeholder={'Full Name'}
+                            {...formik.getFieldProps('fullName')}
+                        />
+                    </div>
+                    </div>
+                    <div>
+                        <b>Looking for a job</b>: {props.profile.lookingForAJob ? 'yes' : 'no'}
+                    </div>
+                    {props.profile.lookingForAJob &&
+                        <div>
+                            <b>My professional skills</b>: {props.profile.lookingForAJobDescription}
+                        </div>
+                    }
+                    <div>
+                        <b>About me</b>: {props.profile.aboutMe}
+                    </div>
+                    <div>
+                        <b>Contacts</b>: {
+
+                    }
+                    </div>
+                </form>
+            </FormikProvider>
         </div>
     )
 }
