c153a6f62eb9cefccc1489dfb1e9fbddd95c762f
Sene4kaaa <senkevich92@mail.ru>
2023-08-09 13:51:30 +0300

    Refactoring code

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import Navbar from "./components/Navbar/Navbar";
-import {Route, withRouter} from "react-router-dom";
+import {Route, Switch, withRouter} from "react-router-dom";
 import News from "./components/News/News";
 import Music from "./components/Music/Music";
 import Settings from "./components/Settings/Settings";
@@ -37,19 +37,21 @@ class App extends React.Component<any> {
 
         return (
             <div className={'app-all'}>
-            <div className={'app-wrapper'}>
-                <HeaderContainer/>
-                <Navbar/>
-                <div className={'app-wrapper-content'}>
-                    <Route path={'/profile/:userId?'} render={withSuspense(ProfileContainer)}/>
-                    <Route path={'/dialogs'} render={withSuspense(DialogsContainer)}/>
-                    <Route path={'/news'} render={() => <News/>}/>
-                    <Route path={'/music'} render={() => <Music/>}/>
-                    <Route path={'/settings'} render={() => <Settings/>}/>
-                    <Route path={'/users'} render={() => <UsersContainer/>}/>
-                    <Route path={'/login'} render={() => <Login/>}/>
+                <div className={'app-wrapper'}>
+                    <HeaderContainer/>
+                    <Navbar/>
+                    <div className={'app-wrapper-content'}>
+                        <Switch>
+                        <Route path={'/profile/:userId?'} render={withSuspense(ProfileContainer)}/>
+                        <Route path={'/dialogs'} render={withSuspense(DialogsContainer)}/>
+                        <Route path={'/news'} render={() => <News/>}/>
+                        <Route path={'/music'} render={() => <Music/>}/>
+                        <Route path={'/settings'} render={() => <Settings/>}/>
+                        <Route path={'/users'} render={() => <UsersContainer/>}/>
+                        <Route path={'/login'} render={() => <Login/>}/>
+                        </Switch>
+                    </div>
                 </div>
-            </div>
             </div>
         );
     }
